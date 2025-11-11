6905f4169036da62e7203dcc3d1313d7fbda093f
Elvis <elvis7t@gmail.com>
2025-11-11 19:28:37 -0300

    clear repository

--- api/src/app/server.ts
+++
@@ -1,6 +0,0 @@
-import app from "./app";
-const PORT =  process.env.PORT;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
--- db/migrations/init-database.sh
+++
@@ -1,1 +0,0 @@
-mysql -u root -proot goomer_lista_rango < "/docker-entrypoint-initdb.d/001-create-tables.sql"
